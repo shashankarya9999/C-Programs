81598db7f20615236da49f3afd31081ee30d2b77
Shashank Arya <shashankarya9831@gmail.com>
2024-03-24 00:49:40 +0530

    Add files via upload

---
+++ C-Programs/13-Structures/pointcoordinates_struct.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+
+struct point
+{
+	float x;
+	float y;
+};
+
+double distance( struct point, struct point );
+
+int main()
+{
+	struct point c1, c2;
+	
+	printf("Enter the x and y coordinates of first point: ");
+	scanf("%f%f",&c1.x,&c1.y);
+
+	printf("Enter the x and y coordinates of second point: ");
+	scanf("%f%f",&c2.x,&c2.y);
+
+	printf("The first co-ordinate: x1 = %f, y1 = %f\n",c1.x,c1.y);
+	printf("The second co-ordinate: x2 = %f, y2 = %f\n",c2.x,c2.y);
+
+	double d = distance( c1,c2 );
+
+	printf("Distance between the two points: %lf\n",d);
+	return 0;  
+}
+
+double distance( struct point z1, struct point z2 )
+{
+	float d, dx, dy;
+	dx = pow( ( z2.x - z1.x ), 2 );
+	dy = pow( ( z2.y - z1.y), 2 );
+	d = sqrt( dy + dx );
+
+	return d;  
+}
---
+++ C-Programs/13-Structures/trianglearea_struct.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+
+struct tri
+{
+	float a;
+	float b;
+	float d;
+};
+
+float area( float a, float b, float d )
+{
+	float A = 0.5*a*b*sin(d);
+	return A;
+}
+
+int main()
+{
+	struct tri t[6];
+
+	t[0].a=137.4, t[0].b=80.90, t[0].d=0.78;
+	t[1].a=155.2, t[1].b=92.62, t[1].d=0.89;
+	t[2].a=149.3, t[2].b=97.63, t[2].d=1.35;
+	t[3].a=160.0, t[3].b=100.25, t[3].d=9.00;
+	t[4].a=155.6, t[4].b=68.95, t[4].d=1.25;
+	t[5].a=149.7, t[5].b=120.0, t[5].d=1.75;
+	
+	float A[6];
+	int i;
+	
+	for( i=0; i<6; i++ )
+	{
+		A[i] = area( t[i].a, t[i].b, t[i].d );
+	}
+
+	for( i=0; i<6; i++ )
+	{
+		printf("Area of %d th triangular land = %f\n",i,A[i]);
+	}  
+
+	float max = A[0];
+	
+	for( i=0; i<6; i++ )
+	{
+		if( A[i]>max )	max = A[i];
+	}
+	
+	printf("The maximum area is: %f\n",max);
+
+	return 0;
+}
