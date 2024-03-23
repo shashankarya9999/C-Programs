ebf1c47c5ad8fbfb9bde6d59a7358b31f2b62e83
Shashank Arya <shashankarya9831@gmail.com>
2024-03-24 00:47:21 +0530

    Add files via upload

---
+++ C-Programs/10-Dynamic-Memory-Allocation/arraymean_pointer_DMA.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+float findMean( int *p, int m );
+
+int main()
+{
+	int *a, i, n;
+	printf("Enter the number of integers: ");
+	scanf("%d",&n);
+
+	a = ( int * )malloc( n*sizeof(int) );
+
+	printf("Enter the integers: ");
+	for( i=0; i<n; i++ )	scanf("%d", a+i);
+
+	printf("The entered array is: ");
+	for( i=0; i<n; i++ )	printf("%d ",*(a+i));
+	printf("\n");
+
+	float ave = findMean( a, n );
+
+	printf("The mean of array elements is %f\n",ave); 
+}
+
+float findMean( int *p, int m )
+{
+	float total = 0.00, avg = 0.00;
+	int j;
+	int low = *( p + 0 );
+
+	for( j=0; j<m; j++ )	total = total + *( p + j );
+
+	avg = total/m;
+	return avg;
+}
---
+++ C-Programs/10-Dynamic-Memory-Allocation/arrayreverse_pointer_DMA.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void reverse( int *b, int m );
+
+int main()
+{
+	int *a, i, n;
+	
+	printf("Enter the number of integers: ");
+	scanf("%d",&n);
+
+	printf("Enter the integers: ");
+
+	a = (int *)malloc( 5*sizeof(int) );	
+
+	for( i=0; i<n; i++ )
+	{
+		scanf("%d", a+i);
+	}
+
+	printf("The entered array is: ");
+
+	for( i=0; i<n; i++ )
+	{
+		printf("%d ", *(a+i));
+	}
+
+	printf("\n");
+	reverse( a,n );
+	
+	return 0;
+}
+
+void reverse( int *b, int m )
+{
+	int j;
+	printf("The array in reverse order is: ");
+
+	for( j=m-1; j>=0; j-- )
+	{
+		printf("%d ",*(b+j));
+	}
+
+	printf("\n");
+}
