b296849c996b45443bae3ba3ba2bebc218d26ef7
Shashank Arya <shashankarya9831@gmail.com>
2024-03-29 19:53:32 +0530

    Update sumproduct.c

--- 01-Basics/sumproduct.c
+++ 01-Basics/sumproduct.c
@@ -1,16 +1,20 @@
+/*
+
+This program asks the user for two integer values then displays their sum and product
+
+*/
+
 #include <stdio.h>
 
 int main()
 
 {
-	/* This program asks the user for two integer values then displays their sum */
 	int value1,value2,sum;
 	printf("\nEnter two integer values:\n");
 	scanf("%d%d",&value1,&value2);
 	sum= value1 + value2;
 	printf("Sum=%d\n",sum);
 	
-	/* This program asks the user for two integer values then displays their product */
 	int num1,num2,product;
 	printf("\nEnter two integer values:\n");
 	scanf("%d%d",&num1,&num2);
@@ -18,5 +22,4 @@ int main()
 	printf("Product=%d\n",product);
 	
 	return 0;
-
 }
