2c8abf70eb88ba7434ba145eb1e820f40115bb81
Shashank Arya <shashankarya9831@gmail.com>
2024-03-30 21:16:03 +0530

    Create storechar_FILE.c

---
+++ 14-File-Handling/storechar_FILE.c
@@ -0,0 +1,33 @@
+/*
+
+This program stores every character typed on the keyboard into a file where the program contiues till the character $ is hit from the keyboard.
+
+*/
+
+#include<stdio.h>
+
+int main()
+{
+  FILE *f1;
+  char x;
+
+  f1 = fopen("data","w");
+  printf("Input the characters");
+
+  while((x = getc(stdin)) != '$')
+  {
+    putc(x,f1);
+    fclose(f1);
+    f1 = fopen("data","r");
+    printf("The input characters are:\n");
+
+    while((x = getc(f1)) != EOF)
+    {
+      putchar(x);
+      getch();
+    }
+  }
+  
+  fclose(f1);
+  return 0;
+}
