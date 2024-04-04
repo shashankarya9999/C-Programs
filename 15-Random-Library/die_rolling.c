3f85c24a510493fdd6930c27421910eb7984f520
Shashank Arya <shashankarya9831@gmail.com>
2024-04-04 07:55:52 +0530

    Add files via upload

---
+++ 15-Random-Library/die_rolling.c
@@ -0,0 +1,31 @@
+/*
+
+Randomising die-rolling program
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i;
+    unsigned seed; //number used to seed random number generator
+
+    printf("Enter Seed: ");
+    scanf("%u",&seed);
+
+    srand(seed); // seed random number generator
+
+    for(i=1; i <= 10; i++)
+    {
+        printf("%10d", 1 + ( rand() % 6 ));
+
+        if(i%5 == 0)
+        {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
