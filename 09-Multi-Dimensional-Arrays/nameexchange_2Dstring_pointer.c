4a38d59cec7175c61494b664f202da860d9ae5a4
Shashank Arya <shashankarya9831@gmail.com>
2024-04-09 18:15:16 +0530

    Update nameexchange_2Dstring_pointer.c

--- 09-Multi-Dimensional-Arrays/nameexchange_2Dstring_pointer.c
+++ 09-Multi-Dimensional-Arrays/nameexchange_2Dstring_pointer.c
@@ -2,21 +2,23 @@
 
 int main()
 {
-	char *names[] = 	 {
-                                        "aatish",
-                                        "shashank",
-                                        "spandan",
-                                        "jolie",
-                                        "priyanshu",
-                                        "hero"
-                                 };
+	char *names[] =  {
+                                "aatish",
+                                "shashank",
+				"spandan",
+                                "jolie",
+                                "priyanshu",
+                                "hero"
+                         };
 
 	char *temp;
 	printf("Original: %s %s\n",names[0],names[1]);
+	
 	temp = names[0];
 	names[0] = names[1];
 	names[1] = temp;
 
 	printf("New: %s %s\n",names[0],names[1]);
+	
 	return 0;
 }
