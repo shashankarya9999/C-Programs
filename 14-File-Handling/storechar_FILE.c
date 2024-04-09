6868ce19aa479290c3b2d3653d490b4c188a3238
Shashank Arya <shashankarya9831@gmail.com>
2024-04-09 18:25:36 +0530

    Update storechar_FILE.c

--- 14-File-Handling/storechar_FILE.c
+++ 14-File-Handling/storechar_FILE.c
@@ -8,27 +8,27 @@ This program stores every character typed on the keyboard into a file where the 
 
 int main()
 {
-  FILE *f1;
-  char x;
-
-  f1 = fopen("data","w");
-  printf("Input the characters");
-
-  while((x = getc(stdin)) != '$')
-  {
-    putc(x,f1);
-    fclose(f1);
-    f1 = fopen("data","r");
-    printf("The input characters are:\n");
-
-    while((x = getc(f1)) != EOF)
-    {
-      putchar(x);
-      getch();
-    }
-  }
-  
-  fclose(f1);
-  
-  return 0;
+      FILE *f1;
+      char x;
+    
+      f1 = fopen("data","w");
+      printf("Input the characters");
+    
+      while( (x = getc(stdin)) != '$')
+      {
+            putc(x,f1);
+            fclose(f1);
+            f1 = fopen("data","r");
+            printf("The input characters are:\n");
+        
+            while((x = getc(f1)) != EOF)
+            {
+                  putchar(x);
+                  getch();
+            }
+      }
+      
+      fclose(f1);
+      
+      return 0;
 }
