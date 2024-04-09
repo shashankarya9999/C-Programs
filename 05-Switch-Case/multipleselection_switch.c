fa909380565be0c00dc1765f5f928dcb602f6535
Shashank Arya <shashankarya9831@gmail.com>
2024-04-09 18:26:36 +0530

    Update multipleselection_switch.c

--- 05-Switch-Case/multipleselection_switch.c
+++ 05-Switch-Case/multipleselection_switch.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 int main()
-
 {
 	int day;
+	
 	printf("Enter the serial number of the day: ");
 	scanf("%d",&day);
 	
@@ -11,18 +11,25 @@ int main()
 	{
 		case 0: printf("Sunday \n");
 			break;
+		
 		case 1: printf("Monday \n");
 			break;
+		
 		case 2: printf("Tuesday \n");
 			break;
+		 
 		case 3: printf("Wednesday \n");
 			break;
+		
 		case 4: printf("Thursday \n");
 			break;
+		
 		case 5: printf("Friday \n");
 			break;
+		
 		case 6: printf("Saturday \n");
 			break;
+		
 		default: printf("Invalid Day \n");
 			 break;
 	}
