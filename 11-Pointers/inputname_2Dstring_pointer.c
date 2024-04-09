19a3d3c596f9b0b84185f7337e40cc06907fc04f
Shashank Arya <shashankarya9831@gmail.com>
2024-04-09 18:19:25 +0530

    Update inputname_2Dstring_pointer.c

--- 11-Pointers/inputname_2Dstring_pointer.c
+++ 11-Pointers/inputname_2Dstring_pointer.c
@@ -4,9 +4,9 @@
 
 int main()
 {
-	char *names[6]; //(2D array of pointers)can take 6 names at most
+	char *names[6]; // (2D array of pointers)can take 6 names at most
 
-	char n[50]; //names are stored in this
+	char n[50]; // names are stored in this
 
 	int len;
 	char *p;
@@ -16,14 +16,15 @@ int main()
 		printf("Enter name: ");	
 		scanf("%s",n);
 		len = strlen(n);
-		p = ( char * )malloc(len+1); //to accommodate '\0'
-		strcpy( p,n );	//copies string p to n
-		names[i] = p;	//base address of name string stored in array of pointers
+		p = (char *)malloc(len+1); // to accommodate '\0'
+		strcpy( p,n );	// copies string p to n
+		names[i] = p;	// base address of name string stored in array of pointers
 	} 
 
 	for(int i=0; i<=5; i++)
 	{
 		printf("%d th names: %s\n",i,names[i]);
 	}
+	
 	return 0;
 }
