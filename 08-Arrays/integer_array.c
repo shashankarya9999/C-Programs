f84d87a75433542f2496ae6c0f1cde612bb198b2
Shashank Arya <shashankarya9831@gmail.com>
2024-04-10 17:04:52 +0530

    Update integer_array.c

--- 08-Arrays/integer_array.c
+++ 08-Arrays/integer_array.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 
 int main()
-
 {
-	int number[6]={ 0,1,2,3,4,5 };
+	int number[6]={ 0, 1, 2, 3, 4, 5 };
 	int i;
 	int max=number[0];
 	int min=number[0];
@@ -12,36 +11,37 @@ int main()
 
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( number[i]>max ) max=number[i];
+		if( number[i] > max )	max=number[i];
 	} 
-	printf("The maximum array element= %d\n",max);
+	printf("The maximum array element = %d\n",max);
 	
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( number[i]<min ) min=number[i];
+		if( number[i] < min )	min=number[i];
 	}
 	printf("The minimum array element= %d\n",min);
 	
 	for( i=0; i < 6; i=i+1 )
 	{
-		sum=sum+number[i];
+		sum = sum + number[i];
 	}
-	avg=sum/6;
-	printf("Average= %f\n",avg);
+	avg = sum / 6;
+	printf("Average = %f\n",avg);
 	
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( i%2 == 0 ) total=total+number[i];
+		if( i % 2 == 0 )	total=total+number[i];
 		else;
 	}
 	printf("Sum of numbers stored at even indices= %f\n",total);
 	
 	printf("Original Array:\n");
-	for( i=0; i < 6;i=i+1 )
+	for( i=0; i < 6; i=i+1 )
 	{
 		printf("%d",number[i]);
 		printf(" ");
 	}
+	
 	printf("\nArray contents in reverse:\n");
 	for( i=5; i>=0; i=i-1 )
 	{
@@ -49,8 +49,6 @@ int main()
 		printf(" ");
 	}
 	printf("\n");	
+	
 	return 0;	
 }
-
-
-
