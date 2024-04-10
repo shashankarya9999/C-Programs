813424240fd4450f8f4db7607556f6c48fcbe53c
Shashank Arya <shashankarya9831@gmail.com>
2024-04-10 16:58:02 +0530

    Update stringlength.c

--- 12-Strings/stringlength.c
+++ 12-Strings/stringlength.c
@@ -17,8 +17,9 @@ int main()
 		i = i + 1;
 	}
 
-	i = i - 1;	//To remove the newline character taken as part of string by fgets function
+	i = i - 1;	// to remove the newline character taken as part of string by fgets function
 
 	printf("The string length is: %d\n",i);
+	
 	return 0;
 }
