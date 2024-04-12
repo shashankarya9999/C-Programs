1fdb5d486a042e03c50314411da4f7572377b07e
Shashank Arya <shashankarya9831@gmail.com>
2024-04-12 10:00:50 +0530

    Update logicaloperators.c

--- 02-Operators/logicaloperators.c
+++ 02-Operators/logicaloperators.c
@@ -14,27 +14,27 @@ int main()
 	e1 = a && b;
 	printf("Value of a AND b is: %d \n",e1);
 	
-	if(e1==0)	printf("False. \n");
+	if(e1 == 0)	printf("False. \n");
 	else		printf("True. \n");
 
 	e2 = a || b;
 	printf("Value of a OR b is: %d \n",e2);
 	
-	if(e2==1) printf("True. \n");
-	else printf("False. \n");
+	if(e2 == 1)	printf("True. \n");
+	else 		printf("False. \n");
 
 	e3 = !!c;
 	
-	if(e3==0) printf("Value of third expression is: %d \nFalse. \n",e3);
-	else printf("Value of third expression is %d \nTrue. \n",e3);
+	if(e3 == 0)	printf("Value of third expression is: %d \nFalse. \n",e3);
+	else 		printf("Value of third expression is %d \nTrue. \n",e3);
 
-	e4= (a)&&(!b);
+	e4 = (a) && (!b);
 	printf("Value of Fourth Expression is %d \n",e4);
 
-	if(e4==0) printf("False. \n");
-	else printf("True. \n");
+	if(e4 == 0)	printf("False. \n");
+	else 		printf("True. \n");
 
-	e5=a<b && b<c;
+	e5 = (a<b) && (b<c);
 	printf("Value of Fifth expression is %d \n",e5);
 
 	if(e5==0) printf("False. \n");
