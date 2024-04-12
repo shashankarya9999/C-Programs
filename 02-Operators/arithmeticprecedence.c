65b26f37f62fa7e204cf64cfb7835b4f469910b6
Shashank Arya <shashankarya9831@gmail.com>
2024-04-12 09:57:58 +0530

    Update arithmeticprecedence.c

--- 02-Operators/arithmeticprecedence.c
+++ 02-Operators/arithmeticprecedence.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 int main()
-
 {
 	int a=1;
 	int b=2;
@@ -15,11 +14,13 @@ int main()
 	exp3= 1 + a * b % c;
 	exp4= a + d % b - c;
 	exp5= e = b = d + c / b - a;
+	
 	printf("First Expression=%d \n",exp1);
 	printf("Second=%d \n",exp2);
 	printf("Third=%d \n",exp3);
 	printf("Fourth=%d \n",exp4);
 	printf("Fifth=%d \n",exp5);
 	printf("Value of e=%d\n",e);
+	
 	return 0;
 }
