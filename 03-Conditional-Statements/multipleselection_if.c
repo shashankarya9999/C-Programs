c542c8fc91c5d8b40210e9c7d602de3402b1717e
Shashank Arya <shashankarya9831@gmail.com>
2024-04-13 11:26:08 +0530

    Update multipleselection_if.c

--- 03-Conditional-Statements/multipleselection_if.c
+++ 03-Conditional-Statements/multipleselection_if.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 int main()
-
 {
 	/* The week starts with sunday(0) and ends with saturday(6) */
 
@@ -9,14 +8,14 @@ int main()
 	printf("Enter the serial number of day: \n");
 	scanf("%d",&day);
 	
-	if( day == 0 ) printf("Sunday \n");
-	if( day == 1 ) printf("Monday \n");
-	if( day == 2 ) printf("Tuesday \n");
-	if( day == 3 ) printf("Wednesday \n");
-	if( day == 4 ) printf("Thursday \n");
-	if( day == 5 ) printf("Friday \n");
-	if( day == 6 ) printf("Saturday \n");
-	if( day < 0 || day > 6 ) printf("Invalid Day \n");
+	if( day == 0 )			printf("Sunday \n");
+	if( day == 1 ) 			printf("Monday \n");
+	if( day == 2 ) 			printf("Tuesday \n");
+	if( day == 3 ) 			printf("Wednesday \n");
+	if( day == 4 ) 			printf("Thursday \n");
+	if( day == 5 ) 			printf("Friday \n");
+	if( day == 6 ) 			printf("Saturday \n");
+	if( day < 0 || day > 6 ) 	printf("Invalid Day \n");
 	
 	return 0;
 }
