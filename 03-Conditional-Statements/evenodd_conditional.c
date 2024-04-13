11c8342f33b18dff2bd07893d6726f9f3d38931e
Shashank Arya <shashankarya9831@gmail.com>
2024-04-13 11:21:24 +0530

    Update evenodd_conditional.c

--- 03-Conditional-Statements/evenodd_conditional.c
+++ 03-Conditional-Statements/evenodd_conditional.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
 int main()
-
 {
 	int a;
+	
 	printf("Enter a number: ");
 	scanf("%d",&a);
+	
 	(a%2 == 0) 
 	? printf("The Number is Even \n") 
 	: printf("The Number is Odd \n");
