33f998aeb5af112fefe99f41a1c40e19a2243768
Shashank Arya <shashankarya9831@gmail.com>
2024-04-15 09:42:26 +0530

    Update gcd_sub.c

--- 01-Basics/gcd_sub.c
+++ 01-Basics/gcd_sub.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int main()
 {
 	int x,y,a,b;
@@ -6,15 +7,17 @@ int main()
 	printf("Enter two numbers in descending order: ");
 	scanf("%d%d",&a,&b);
 
-	x=a;
-	y=b;
+	x = a;
+	y = b;
 
-	while( x!=y )
+	while( x != y )
 	{
-		if( x>y ) x=x-y;
-		else y=y-x;
+		if( x > y )	x = x - y;
+		else 		y = y - x;
 	}
+	
 	printf("The GCD of %d and %d is %d.\n",a,b,x);
+	
 	return 0;
 }
 
