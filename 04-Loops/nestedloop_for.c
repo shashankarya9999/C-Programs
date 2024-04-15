25e09dc57e0f337cd28c7a68202819b1e5908ddc
Shashank Arya <shashankarya9831@gmail.com>
2024-04-15 15:28:18 +0530

    Update nestedloop_for.c

--- 04-Loops/nestedloop_for.c
+++ 04-Loops/nestedloop_for.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 int main()
-
 {
 	int i,j;
 	
@@ -9,12 +8,13 @@ int main()
 	{
 		for( j=1; j<3; j=j+1 )
 		{
-			if( j%2 == 0 ) printf("O");
-			else printf("X");
+			if( j % 2 == 0 )	printf("O");
+			else 			printf("X");
 		}
 	
 	printf("\n");
 	}
+	
 	return 0;
 }
 
