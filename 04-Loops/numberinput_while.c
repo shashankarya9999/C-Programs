96214d66ec028aabb3afce970e9c72082cb57cc0
Shashank Arya <shashankarya9831@gmail.com>
2024-04-15 15:29:18 +0530

    Update numberinput_while.c

--- 04-Loops/numberinput_while.c
+++ 04-Loops/numberinput_while.c
@@ -6,25 +6,29 @@ int main()
 
 	do 
 	{
-		printf("Enter another number(Enter 67 to end): ");	//Sentinel value is 67(Ascii for C)
+		printf("Enter another number(Enter 67 to end): ");	// sentinel value is 67(Ascii for C)
 		scanf("%d",&num);
 
 		if( num == 0 )
 		{
 			count_z++;
 		}
+			
 		else if( num > 0 )	
 		{
 			count_p++;
 		}
+			
 		else	
 		{
 			count_n++;
 		}
+		
 	} while( num != 67 );
 
 	printf("Number of positive numbers entered = %d\n",count_p);
 	printf("Number of negative numbers entered = %d\n",count_n);
 	printf("Number of zeroes entered = %d\n",count_z);
+	
 	return 0;	
 }
