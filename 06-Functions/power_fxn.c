4e468523ad73a965216dcc9aae6ec734c2f28321
Shashank Arya <shashankarya9831@gmail.com>
2024-04-16 22:13:02 +0530

    Update power_fxn.c

--- 06-Functions/power_fxn.c
+++ 06-Functions/power_fxn.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int power( int , int );
+int power( int, int );
 
 int main()
 {
@@ -10,16 +10,18 @@ int main()
 	printf("Enter b: ");
 	scanf("%d",&b);
 	printf("%d ^ %d = %d\n",a,b,power(a,b));
+	
 	return 0;
 }
 
 int power( int a, int b )
 {
-	int result=1;
-	while( b!=0 )
+	int result = 1;
+	while( b != 0 )
 	{
-		result = result*a;
+		result = result * a;
 		b--;	
 	}
+	
 	return (result);
 }   
