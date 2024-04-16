d1cf2bebe86d29f281927299770401a9cdd7552f
Shashank Arya <shashankarya9831@gmail.com>
2024-04-16 22:13:28 +0530

    Update numreverse_fxn.c

--- 06-Functions/numreverse_fxn.c
+++ 06-Functions/numreverse_fxn.c
@@ -18,11 +18,11 @@ void reverseNum( int n )
 {
 	int rem, reverse=0;
 
-	while( n!=0 )
+	while( n != 0 )
 	{
-		rem = n%10;
-		reverse = reverse*10 + rem;
-		n = n/10;
+		rem = n % 10;
+		reverse = reverse * 10 + rem;
+		n = n / 10;
 	}
 
 	printf("Reversed Number: %d\n",reverse);
