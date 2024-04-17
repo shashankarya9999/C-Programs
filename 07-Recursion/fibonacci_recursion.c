72ca419ffb5668b063b2da38faa8411927c11203
Shashank Arya <shashankarya9831@gmail.com>
2024-04-17 14:03:58 +0530

    Update fibonacci_recursion.c

--- 07-Recursion/fibonacci_recursion.c
+++ 07-Recursion/fibonacci_recursion.c
@@ -11,19 +11,18 @@ int main()
 
 	printf("The fibonacci series is as follows:\n");
 	
-	for( i=0; i<n; i++ )
-	{
-		printf(" %d ",fibo(i));
-	}
+	for( i=0; i<n; i++ )	printf(" %d ",fibo(i));
+	
 	printf("\n");
+	
 	return 0;
 }
 
 int fibo( int num )
 {
-	if( num == 0 )	return 0;
+	if( num == 0 )		return 0;
 
 	else if ( num == 1 )	return 1;
 
-	else	return ( fibo( num-1 ) + fibo( num-2 ) );
+	else			return ( fibo( num - 1 ) + fibo( num - 2 ) );
 }
