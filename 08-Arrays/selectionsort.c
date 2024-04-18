fb384db6288208eaf5ca8ff14a264f6ca10cd97f
Shashank Arya <shashankarya9831@gmail.com>
2024-04-18 13:19:38 +0530

    Update selectionsort.c

--- 08-Arrays/selectionsort.c
+++ 08-Arrays/selectionsort.c
@@ -3,7 +3,6 @@
 void selection_sort( int [], int );
 
 int main()
-
 {
 	int a[10],n,i,j;
 
@@ -12,25 +11,17 @@ int main()
 
 	printf("Enter the integers: ");
 
-	for( i=0; i<n; i=i+1 )
-	{
-		scanf("%d",&a[i]);
-	}
+	for( i=0; i<n; i=i+1 )	scanf("%d",&a[i]);
 
 	printf("Array : ");
-	for( i=0; i<n; i++ )
-	{
-		printf("%d ",a[i]);
-	}
+	for( i=0; i<n; i++ )	printf("%d ",a[i]);
+	
 	printf("\n");
 
 	selection_sort( a,n );
 
 	printf("The sorted array is: ");
-	for( j=0; j<n; j=j+1 )
-	{
-		printf("%d ", a[j]);
-	}
+	for( j=0; j<n; j=j+1 )	printf("%d ", a[j]);
 
 	printf("\n");
 
@@ -42,12 +33,14 @@ void selection_sort( int a[], int m )
 	for( int i=0; i<m; i++ )
 	{
 		int min = i;
+		
 		for( int j=i+1; j<m; j++ )
 		{
 			if( a[min] > a[j] )	min = j;
 		}
-	int temp = a[i];
-	a[i] = a[min];
-	a[min] = temp;
+		
+		int temp = a[i];
+		a[i] = a[min];
+		a[min] = temp;
 	}  
 }
