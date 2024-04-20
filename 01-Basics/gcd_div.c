29ef5e7156721a9420794816dafd5bfaaa4fb08a
Shashank Arya <shashankarya9831@gmail.com>
2024-04-20 20:49:15 +0530

    Update gcd_div.c

--- 01-Basics/gcd_div.c
+++ 01-Basics/gcd_div.c
@@ -3,17 +3,18 @@
 int main()	// euclid's algorithm
 {
 	int x,y,a,b,r;
+	
 	printf("Enter two numbers in descending order: ");
 	scanf("%d%d",&a,&b);
 
 	x=a;
 	y=b;
 	
-	while( y!=0 )
+	while( y != 0 )
 	{
-		r=x%y;
-		x=y;
-		y=r;
+		r = x % y;
+		x = y;
+		y = r;
 	}
 	
 	printf("The GCD of %d and %d is %d\n",a,b,x);
