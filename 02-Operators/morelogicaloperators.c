636a9d6a7c43a5f3fd17926a9a5a18a25e2e9334
Shashank Arya <shashankarya9831@gmail.com>
2024-04-20 20:52:28 +0530

    Update morelogicaloperators.c

--- 02-Operators/morelogicaloperators.c
+++ 02-Operators/morelogicaloperators.c
@@ -14,20 +14,20 @@ int main()
 	e4 = (b<c) && (c==7);
 	e5 = (c+1-b == 0) || (b=5);
 	
-	if(e1==1) printf("True. \n");
-	else printf("False. \n");
+	if( e1 == 1 )	printf("True. \n");
+	else 		printf("False. \n");
 
-	if(e2==1) printf("True. \n");
-        else printf("False. \n");
+	if( e2 == 1 ) 	printf("True. \n");
+        else 		printf("False. \n");
 
-	if(e3==1) printf("True. \n");
-        else printf("False. \n");
+	if( e3 == 1 ) 	printf("True. \n");
+        else 		printf("False. \n");
 
-	if(e4==1) printf("True. \n");
-        else printf("False. \n");
+	if( e4 == 1 ) 	printf("True. \n");
+        else 		printf("False. \n");
 
-	if(e5==1) printf("True. \n");
-        else printf("False. \n");
+	if( e5 == 1 ) 	printf("True. \n");
+        else 		printf("False. \n");
 
 	return 0;
 }
