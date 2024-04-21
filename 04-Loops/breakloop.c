0844500ae732fed7d65cb31749f6b356f413dcff
Shashank Arya <shashankarya9831@gmail.com>
2024-04-21 12:08:38 +0530

    Update breakloop.c

--- 04-Loops/breakloop.c
+++ 04-Loops/breakloop.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 
 int main()
-
 {
 	int i;
 	
 	for( i=1; i<10; i=i+1 )
 	{
-		if( i==5 )
-		{ 
+		if( i == 5 )
+		{
 			break;
 		}
 		printf("%d",i);
 		printf(" ");
 	}
+	
 	printf("\nBroke out of the loop at i=%d \n",i);
+	
 	return 0;
 }
