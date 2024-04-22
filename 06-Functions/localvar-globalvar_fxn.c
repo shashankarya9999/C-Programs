a0622ddad3cddd29920b5bfdb0d6ca99234d9b3b
Shashank Arya <shashankarya9831@gmail.com>
2024-04-22 14:00:37 +0530

    Update localvar-globalvar_fxn.c

--- 06-Functions/localvar-globalvar_fxn.c
+++ 06-Functions/localvar-globalvar_fxn.c
@@ -3,17 +3,17 @@
 void addOne( int num1 );
 
 int main()
-
 {
 	int num1=5;
-	addOne( num1 );
+	
+	addOne(num1);
 	printf("In Main: num1 = %d\n",num1);
+	
 	return 0;
 }
 
 void addOne( int num1 )
-
 {
-	num1=num1+1;
+	num1 = num1 + 1;
 	printf("In addOne: num1 = %d\n",num1);
 }
