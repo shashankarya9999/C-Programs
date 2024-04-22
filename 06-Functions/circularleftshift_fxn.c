7ef7836bdaa90ffcf4ac0eab63c2fcd46629cb59
Shashank Arya <shashankarya9831@gmail.com>
2024-04-22 13:53:44 +0530

    Update circularleftshift_fxn.c

--- 06-Functions/circularleftshift_fxn.c
+++ 06-Functions/circularleftshift_fxn.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 void swap( int *, int * );
-void circular_swap( int *);
+void circular_swap( int * );
 
 int main()
 {
         int p[5];
-        printf("Enter five numbers: ");
+        
+	printf("Enter five numbers: ");
         for( int i=0; i<5; i++ )
 	{
 		scanf("%d",&p[i]);
@@ -17,9 +18,11 @@ int main()
 	{
         	printf("%d ",p[i]);
 	}
+	
 	printf("\n");
 
-        circular_swap( p );
+        circular_swap(p);
+	
         return 0;
 }
 
@@ -33,15 +36,16 @@ void swap( int *x, int *y )
 
 void circular_swap( int *p )
 {
-	swap( (p),(p+2) );
-        swap( (p+1),(p+4) );
-        swap( (p+2),(p+3) );
-        swap( (p+1),(p+2) );
+	swap( (p), (p+2) );
+        swap( (p+1), (p+4) );
+        swap( (p+2), (p+3) );
+        swap( (p+1), (p+2) );
 
 	printf("After shift: ");
         for( int i=0; i<5; i++ )
         {
                 printf("%d ",p[i]);
         }
+	
 	printf("\n");
 }
