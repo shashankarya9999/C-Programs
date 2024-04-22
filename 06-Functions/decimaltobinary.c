48af1e7bc33ab3f7bdd347e014bb7fab35a5f576
Shashank Arya <shashankarya9831@gmail.com>
2024-04-22 13:57:08 +0530

    Update decimaltobinary.c

--- 06-Functions/decimaltobinary.c
+++ 06-Functions/decimaltobinary.c
@@ -21,7 +21,7 @@ int main()
 	
 	if( dec_num > pow(2,n) - 1 )	printf("Input Decimal Number is out of range.\n");
 	
-	else	convert( dec_num, n );
+	else				convert( dec_num, n );
 	
 	return 0;
 }
@@ -32,18 +32,18 @@ void convert( int dec_num, int n )
 	int i=0;
 	int j;
 
-	while ( dec_num != 0 )
+	while( dec_num != 0 )
 	{
 		rems[i] = dec_num % 2;
 		dec_num = dec_num / 2;
-		i=i+1;
+		i = i + 1;
 	}
 	
 	printf("The binary equivalent of the equivalent decimal number is: ");
-	
 	for( j=i-1; j>=0; j-- )
 	{
 		printf(" %d ",rems[j]);
 	}
+	
 	printf("\n");
 }
