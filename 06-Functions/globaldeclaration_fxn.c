7d37b8b2553fbc242cfbf6cc9b3dfc47fb45cd1f
Shashank Arya <shashankarya9831@gmail.com>
2024-04-22 14:00:12 +0530

    Update globaldeclaration_fxn.c

--- 06-Functions/globaldeclaration_fxn.c
+++ 06-Functions/globaldeclaration_fxn.c
@@ -5,12 +5,11 @@ float average; // Global Declaration of average(It is accessible by both main an
 void averageTwo( int num1, int num2 ); // Function Prototype
 
 int main()
-
 {
 	int num1=5;
 	int num2=8;
 	
-	averageTwo(num1,num2); // Function averageTwo called by main function 
+	averageTwo( num1, num2 ); // Function averageTwo called by main function 
 
 	printf("The average of %d and %d is %f\n",num1,num2,average);
 	
@@ -20,9 +19,8 @@ int main()
 // Definition of averageTwo function begins 
 
 void averageTwo(int num1,int num2) // averageTwo function header
-
 {
-	average=(float) (num1+num2)/2;	// statement of averageTwo function
+	average = (float) ( num1 + num2 ) / 2;	// statement of averageTwo function
 }
 
 
