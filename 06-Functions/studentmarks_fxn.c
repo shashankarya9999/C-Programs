0a9da8a0111e6da1ba947ddab2139d4b5ef00710
Shashank Arya <shashankarya9831@gmail.com>
2024-04-24 14:43:55 +0530

    Update studentmarks_fxn.c

--- 06-Functions/studentmarks_fxn.c
+++ 06-Functions/studentmarks_fxn.c
@@ -9,22 +9,27 @@ int main()
 	printf("Enter marks in three subjects: ");
 	scanf("%d%d%d",&m1,&m2,&m3);
 
-	if( (m1<0 || m1>100) && (m2<0 || m2>100) && (m3<0 || m3>100) )	printf("Invalid Input!\n");
+	if( ( m1 < 0 || m1 > 100 ) && ( m2 < 0 || m2 > 100 ) && ( m3 < 0 || m3 > 100 ) )	
+	{	
+		printf("Invalid Input!\n");
+	}
+		
 	else
 	{
-		avg( m1,m2,m3 );
-		percent( m1,m2,m3 );
+		avg( m1, m2, m3 );
+		percent( m1, m2, m3 );
 	}
+	
 	return 0;
 }
 
 void avg( int m1, int m2, int m3 )
 {
-	printf("Average = %f\n",(float)((m1+m2+m3)/3));
+	printf("Average = %f\n",(float)( ( m1 + m2 + m3 ) / 3) );
 }
 
 void percent( int m1, int m2, int m3 )
 {
-	printf("Percentage = %f %%\n",(float)((m1+m2+m3)/3));
+	printf("Percentage = %f %%\n",(float)( ( m1 + m2 + m3 ) / 3) );
 }
 
