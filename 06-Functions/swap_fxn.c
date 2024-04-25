008640f8a25ad7676a656991bbeab3e19b4f9f59
Shashank Arya <shashankarya9831@gmail.com>
2024-04-25 13:08:46 +0530

    Update swap_fxn.c

--- 06-Functions/swap_fxn.c
+++ 06-Functions/swap_fxn.c
@@ -13,7 +13,7 @@ int main()
 	printf("Entered values are: a = %d, b = %d\n",a,b);
 
 	tempvar_swap( a, b );
-	notempvar_swap( a,b );
+	notempvar_swap( a, b );
 	
 	printf("The values in main function: a = %d, b = %d\n",a,b);
 	
@@ -23,18 +23,18 @@ int main()
 void tempvar_swap( int x, int y )
 {
 	int temp;
-	temp=x;
-	x=y;
-	y=temp;
+	temp = x;
+	x = y;
+	y = temp;
 
 	printf("Swapping with temporary variable: a = %d, b = %d\n",x,y);
 }
 
 void notempvar_swap( int p, int q )
 {
-	p=p+q;
-	q=p-q;
-	p=p-q;
+	p = p + q;
+	q = p - q;
+	p = p - q;
 
 	printf("Swapping without temporary variable: a = %d, b = %d\n",p,q);
 }
