68f41382fe72afcac7332b87d96860b35e945c3e
Shashank Arya <shashankarya9831@gmail.com>
2024-04-26 09:27:20 +0530

    Update upperlowerspecialch_string.c

--- 12-Strings/upperlowerspecialch_string.c
+++ 12-Strings/upperlowerspecialch_string.c
@@ -9,64 +9,69 @@ int main()
 	char str[]="My name is Shashank Arya!\n";
 	printf("%s",str);
 
-	int uppercount= uppercasecount(str);
-	int lowercount= lowercasecount(str);
-	int specialcount= specialchcount(str);
+	int uppercount = uppercasecount(str);
+	int lowercount = lowercasecount(str);
+	int specialcount = specialchcount(str);
 
 	printf("Uppercase letter: %d\n",uppercount);
 	printf("Lowercase letter: %d\n",lowercount);
 	printf("Special characters: %d\n",specialcount);
+
+	return 0;
 }
 
 int uppercasecount( char *a )
 {
-	int counter = 0;
+	int u_counter = 0;
 	int i=0;
 
 	while( a[i] != '\0' )
 	{
 		if( a[i] >= 'A' && a[i] <= 'Z' )
 		{
-			counter = counter + 1;
+			u_counter = u_counter + 1;
 		}
+		
 		i++;
 	}
 	
-	return counter;
+	return u_counter;
 }
 
 int lowercasecount( char *a )
 {
-        int counter = 0;
+        int l_counter = 0;
         int i=0;
 
         while( a[i] != '\0' )
         {
                 if( a[i] >= 'a' && a[i] <= 'z' )
                 {
-                        counter = counter + 1;
+                        l_counter = l_counter + 1;
                 }
+		
                 i++;
         }
 	
-        return counter;
+        return l_counter;
 }
 
 int specialchcount( char *a )
 {
-	int counter = 0;
+	int s_counter = 0;
 	int i=0;
 
 	while( a[i] != '\0' )
 	{
 		if( !((a[i] >= 'A' && a[i] <= 'Z')||(a[i] >= 'a' && a[i] <= 'z')) )
 		{
-			counter = counter + 1;
+			s_counter = s_counter + 1;
 		}
+		
 		i++;
 	}
 	
-	return counter;
+	return s_counter;
 }
 
 
