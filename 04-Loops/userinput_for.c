625db1b9983a1550b7ef3ff25006fc195259a930
Shashank Arya <shashankarya9831@gmail.com>
2024-04-28 05:21:06 +0530

    Update userinput_for.c

--- 04-Loops/userinput_for.c
+++ 04-Loops/userinput_for.c
@@ -7,22 +7,17 @@ int main()
 	printf("Enter a positive number: \n");
 	scanf("%d",&num);	
 
-	if( num == 0 ) printf("The number you entered is zero. \n");
+	if( num == 0 )	printf("The number you entered is zero. \n");
 	
 	else
 	{	
-	for( ; num < 0 ; )
-	{
-		printf("That is not a positive number. \n");
-		printf("Enter another positive number: \n");
-		scanf("%d",&num);
-	}
+		for( ; num < 0; )
+		{
+			printf("That is not a positive number. \n");
+			printf("Enter another positive number: \n");
+			scanf("%d",&num);
+		}
 	}
 
-	if(num==0);
-	else	{
-		printf("The number you entered is %d. \n",num);
-		}
-	
 	return 0;
 }
