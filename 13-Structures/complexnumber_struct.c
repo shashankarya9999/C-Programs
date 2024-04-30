453f3fac30bab054d2682ede2bbd424f610dfb6f
Shashank Arya <shashankarya9831@gmail.com>
2024-05-01 04:54:06 +0530

    Update complexnumber_struct.c

--- 13-Structures/complexnumber_struct.c
+++ 13-Structures/complexnumber_struct.c
@@ -16,12 +16,12 @@ int main()
 	printf("Enter the real and imaginary part of first complex number: ");
 	scanf("%f%f",&z1.a,&z1.b);
 
-	complex_represent( z1 );
+	complex_represent(z1);
 
 	printf("Enter the real and imaginary part of second complex number: ");
         scanf("%f%f",&z2.a,&z2.b);
 
-	complex_represent( z2 );
+	complex_represent(z2);
 	
 	complex_add( z1, z2 );
 	
@@ -35,5 +35,5 @@ void complex_represent( struct complex z )
 
 void complex_add( struct complex w1, struct complex w2 )
 {
-	printf("The sum is: %f + %fi\n",(w1.a + w2.a),(w1.b + w2.b));
+	printf("The sum is: %f + %fi\n",( w1.a + w2.a ),( w1.b + w2.b ));
 }
