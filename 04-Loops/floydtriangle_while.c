2c4d51f01d43b20d3359f222698632e8434abf84
Shashank Arya <shashankarya9831@gmail.com>
2024-05-05 07:13:31 +0530

    Update floydtriangle_while.c

--- 04-Loops/floydtriangle_while.c
+++ 04-Loops/floydtriangle_while.c
@@ -3,27 +3,31 @@
 int main()
 
 {
-	int row=1;
+	int row = 1;
 	int col,num;
-	int number=1;
+	int number = 1;
 	
 	printf("Enter the number of rows: ");
 	scanf("%d",&num);
-	if( num==0 )	printf("No Triangle! \n");
-	else printf("Your Triangle! \n");
+	
+	if( num == 0 )	printf("No Triangle! \n");
+	else 		printf("Your Triangle! \n");
 
 	while( row <= num )
 	{
-		col=1;
+		col = 1;
+		
 		while( col <= row )
 		{
 			printf("%d",number);
 			printf(" ");
-			col=col+1;
-			number=number+1;
+			col = col + 1;
+			number = number + 1;
 		}
+		
 	printf("\n");
-	row=row+1;
+	row = row + 1;
 	}
+	
 	return 0;	
 }
