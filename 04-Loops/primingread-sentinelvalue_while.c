7c9e20254e221abd28e6bfd8d61c494ff48460da
Shashank Arya <shashankarya9831@gmail.com>
2024-05-05 07:20:51 +0530

    Update primingread-sentinelvalue_while.c

--- 04-Loops/primingread-sentinelvalue_while.c
+++ 04-Loops/primingread-sentinelvalue_while.c
@@ -4,8 +4,8 @@ int main()
 {
 	int counter,score,total,average;
 
-	total=0;
-	counter=1;
+	total = 0;
+	counter = 1;
 	
 	/* Priming Read */
 
@@ -16,14 +16,14 @@ int main()
 
 	while( score != -2 )
 	{
-		total=total+score;
-		counter = counter+1;
+		total = total + score;
+		counter = counter + 1;
 
 		printf("Enter another number: ");
 		scanf("%d",&score);
 	}
 
-	average = total/counter;
+	average = total / counter;
 	printf("Class average is %d \n",average);
 	
 	return 0;
