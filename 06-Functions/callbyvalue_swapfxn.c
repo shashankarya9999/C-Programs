cf1036390fd5a7a357c1f22be3e2c83979ac7c8c
Shashank Arya <shashankarya9831@gmail.com>
2024-05-05 07:40:56 +0530

    Update callbyvalue_swapfxn.c

--- 06-Functions/callbyvalue_swapfxn.c
+++ 06-Functions/callbyvalue_swapfxn.c
@@ -6,17 +6,17 @@ Here, no swapping of values of num1 and num2 occurs because changes to local var
 
 #include<stdio.h>
 
-void swap( int num1, int num2 ); // Function Prototype
+void swap( int num1, int num2 ); // function prototype
 
 int main()
 {
-	int num1=20;
-	int num2=30;
+	int num1 = 20;
+	int num2 = 30;
 
 	printf("num1 and num2 before call to swap:\n");
 	printf("num1=%d, num2=%d\n",num1,num2);
 
-	swap( num1, num2 ); // Swap function called by main function
+	swap( num1, num2 ); // swap function called by main function
 	
 	printf("num1 and num2 after call to swap:\n");
 	printf("num1=%d, num2=%d\n",num1,num2);
@@ -24,7 +24,7 @@ int main()
 	return 0;
 }
 
-void swap( int num1, int num2 ) // Swap function declaration
+void swap( int num1, int num2 ) // swap function declaration
 {
 	int temp;
 	temp = num1;
