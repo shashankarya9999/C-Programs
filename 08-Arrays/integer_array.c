72080d1b56c2f081e874879c171aa675bd7d7829
Shashank Arya <shashankarya9831@gmail.com>
2024-05-07 23:59:25 +0530

    Update integer_array.c

--- 08-Arrays/integer_array.c
+++ 08-Arrays/integer_array.c
@@ -4,20 +4,20 @@ int main()
 {
 	int number[6]={ 0, 1, 2, 3, 4, 5 };
 	int i;
-	int max=number[0];
-	int min=number[0];
-	float sum=0;
+	int max = number[0];
+	int min = number[0];
+	float sum = 0;
 	float avg,total=0;
 
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( number[i] > max )	max=number[i];
+		if( number[i] > max )	max = number[i];
 	} 
 	printf("The maximum array element = %d\n",max);
 	
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( number[i] < min )	min=number[i];
+		if( number[i] < min )	min = number[i];
 	}
 	printf("The minimum array element= %d\n",min);
 	
@@ -30,7 +30,7 @@ int main()
 	
 	for( i=0; i < 6; i=i+1 )
 	{
-		if( i % 2 == 0 )	total=total+number[i];
+		if( i % 2 == 0 )	total = total + number[i];
 		else;
 	}
 	printf("Sum of numbers stored at even indices= %f\n",total);
