a4d71df40b934d00d2998485f5bb25b50f2732b4
Shashank Arya <shashankarya9831@gmail.com>
2024-05-18 00:09:19 +0530

    Update morelogicaloperators.c

--- 02-Operators/morelogicaloperators.c
+++ 02-Operators/morelogicaloperators.c
@@ -3,16 +3,15 @@
 #include<stdio.h>
 
 int main()
-
 {
-	int a=5,b=7,c=17;
-	int e1,e2,e3,e4,e5;
+	int a = 5,b = 7,c = 17;
+	int e1, e2, e3, e4, e5;
 	
-	e1 = c/b == 2;
-	e2 = c%b <= a%b;
-	e3 = b+c/a != c-a;
-	e4 = (b<c) && (c==7);
-	e5 = (c+1-b == 0) || (b=5);
+	e1 = c / b == 2;
+	e2 = c % b <= a % b;
+	e3 = b + c / a != c - a;
+	e4 = (b < c) && (c == 7);
+	e5 = (c + 1 - b == 0) || (b = 5);
 	
 	if( e1 == 1 )	printf("True. \n");
 	else 		printf("False. \n");
