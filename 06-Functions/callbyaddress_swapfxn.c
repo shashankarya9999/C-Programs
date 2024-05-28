f6fabd3f9102fa8c8f082f020355a9fda8c0688b
Shashank Arya <shashankarya9831@gmail.com>
2024-05-28 13:46:04 +0530

    Update callbyaddress_swapfxn.c

--- 06-Functions/callbyaddress_swapfxn.c
+++ 06-Functions/callbyaddress_swapfxn.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-void swap( int *p, int *q ); // Function Prototype
+void swap( int *p, int *q );	// Function Prototype
 
 int main()
 {
@@ -8,18 +8,17 @@ int main()
         int num2 = 30;
 
         printf("num1 and num2 before call to swap:\n");
-        printf("num1=%d, num2=%d\n",num1,num2);
+        printf("num1 = %d, num2 = %d\n",num1,num2);
 
-        swap( &num1, &num2 ); // swap function called by main function
+        swap( &num1, &num2 );	// swap function called by main function
 
         printf("num1 and num2 after call to swap:\n");
-        printf("num1=%d, num2=%d\n",num1,num2);
+        printf("num1 = %d, num2 = %d\n",num1,num2);
 
         return 0;
 }
 
 void swap( int *p, int *q )
-
 {
 	int temp;
 	temp = *p;
