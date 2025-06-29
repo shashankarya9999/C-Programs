58734ef5adc5c7320394574832f2dfdeb55b5473
Shashank Arya <shashankarya9831@gmail.com>
2025-06-29 12:41:56 +0530

    Update addmats_2_fxn.c

--- 06-Functions/addmats_2_fxn.c
+++ 06-Functions/addmats_2_fxn.c
@@ -1,8 +1,4 @@
-/*
-
-This program takes in two 2x2 matrices as input; and then uses a function called addmats to compute and print the sum of the two matrices.
-
-*/
+// This program takes in two 2x2 matrices as input; and then uses a function called addmats to compute and print the sum of the two matrices
 
 #include<stdio.h>
 
