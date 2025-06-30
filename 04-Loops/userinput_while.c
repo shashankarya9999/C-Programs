c16ff21e34f07c39f7c7459cf6dc04008dc7329b
Shashank Arya <shashankarya9831@gmail.com>
2025-06-30 12:22:48 +0530

    Update userinput_while.c

--- 04-Loops/userinput_while.c
+++ 04-Loops/userinput_while.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-	/* This program takes user input and checks if it is positive or negative or zero. If it is zero, we get two statements saying it is zero. If the number is positive, we right away get a statement which says the number is <number> and if the number is negative, we get an error message and a statement which gets another input from user. This continues until we get a positive number as user input(not zero, as it is a different case here)  */
+	/* This program takes user input and checks if it is positive or negative or zero. 
+ 	If it is zero, we get two statements saying it is zero. 
+  	If the number is positive, we right away get a statement which says the number is <number> 
+   	and if the number is negative, we get an error message and a statement which gets another input from user. 
+    	This continues until we get a positive number as user input(not zero, as it is a different case here)  */
 	
 	int number;
 	printf("Enter a Positive Integer: \n");
